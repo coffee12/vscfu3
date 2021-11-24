33f0a4261ee1cc7e44437bb57b0c30146b7e73d1
coffee <rb55735@gmail.com>
2021-11-24 13:08:05 -0500

    Defines
    performance and QOL

---
+++ common/defines/00_coffeevictor.lua
@@ -0,0 +1,41 @@
+---Optimization and Graphics
+NDefines.NGame.DAYS_BEHIND_PAUSE = 90
+NDefines.NGame.DAYS_BEHIND_LOWER_SPEED = 45
+NDefines.NEngine.EVENT_PROCESS_OFFSET = 35
+NDefines.NGraphics.CAPITAL_INDICATOR_HEIGHT_SCALE = 100.0
+NDefines.NGraphics.CAPITAL_INDICATOR_HEIGHT = 0
+NDefines.NGraphics.BORDER_WIDTH = 1
+NDefines.NGraphics.DIRECTION_POINTER_DRAW_DISTANCE = 1000
+NDefines.NGraphics.DIRECTION_POINTER_INTERPOLATION_SPEED = 0.1
+
+--- AI
+NDefines.NAI.DEVELOPMENT_CAP_BASE = 30
+
+--- Diplo
+NDefines.NDiplomacy.MIN_RELATIONS_TO_ALLY = -200
+NDefines.NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION = 12
+NDefines.NDiplomacy.CONDOTTIERI_MIN_DURATION = 1
+NDefines.NDiplomacy.CHANGE_RIVAL_YEARS = 10
+NDefines.NDiplomacy.HRE_AUTHORITY_JOIN_EMPIRE = 0
+NDefines.NDiplomacy.GREAT_POWER_SUBJECT_CONTRIBUTION = 0
+
+---Country
+NDefines.NCountry.MONARCH_MIN_SKILL = 1
+NDefines.NCountry.NAT_FOCUS_YEARS = 20
+NDefines.NCountry.EXPLOIT_COOLDOWN_MONTHS = 120
+NDefines.NCountry.ABDICATE_RULING_LENGTH_THRESHOLD = 10
+NDefines.NCountry.ABDICATE_PRESTIGE_HIT = -50
+NDefines.NCountry.DISINHERIT_PRESTIGE_HIT = -30
+NDefines.NCountry.DISINHERIT_PRESTIGE_THRESHOLD = -30
+NDefines.NCountry.CALL_ALLY_DECLINE_PRESTIGE_PENALTY = 0
+
+--NMilitary
+NDefines.NMilitary.WARGOAL_MAX_BONUS = 40
+
+-- Economy
+NDefines.NEconomy.CARAVAN_POWER_MAX = 25
+NDefines.NEconomy.CARAVAN_POWER_MIN = 1
+
+package.loaded["debug"] = nil
+_G["debug"] = nil
+debug = nil
